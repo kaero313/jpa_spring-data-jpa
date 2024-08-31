64ddb28ac18e8c50c61d5a0bfcd7ce361ea0c59a
HOME <kaero313@naver.com>
2024-08-31 20:36:16 +0900

    단건 save 구현, 데이터 받아서 처리

--- src/main/java/jpa/jpaController.java
+++ src/main/java/jpa/jpaController.java
@@ -3,8 +3,12 @@ package jpa;
 import jpa.member.dto.member;
 import jpa.member.service.memberService;
 import org.assertj.core.util.Lists;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -22,7 +26,7 @@ public class jpaController {
     @RequestMapping(value = "member/select/all", method = {RequestMethod.POST})
     public List<member> select_all(HttpServletRequest request){
 
-        List<member> members = memberService.findAll();
+        List<member> members = memberService.findAll(Sort.by(Sort.Direction.ASC, "name"));
 
         System.out.println(members);
 
@@ -30,17 +34,39 @@ public class jpaController {
     }
 
     @RequestMapping(value = "member/select/where", method = {RequestMethod.POST})
-    public List<member> select_where(HttpServletRequest request){
+    public List<member> select_where(@RequestBody String request){
 
-        List<String> lists = new ArrayList<>();
-        lists.add("id1");
-        lists.add("id55");
+        JSONArray jsonary = new JSONArray(request);
 
-        List<member> members = memberService.findAllById(lists);
+        List<String> ids = new ArrayList<>();
+
+        for(int i=0; i<jsonary.length(); i++){
+            JSONObject jo = jsonary.getJSONObject(i);
+            System.out.println(jo.getString("id"));
+            ids.add(jo.getString("id"));
+        }
+
+        List<member> members = memberService.findAllById(ids);
 
         System.out.println(members);
 
         return members;
     }
+
+
+    @RequestMapping(value = "member/save/one", method = {RequestMethod.POST})
+    public void save_one(@RequestBody String request){
+
+        JSONObject jsonobj = new JSONObject(request);
+
+        member member = new member();
+        member.setId(jsonobj.getString("id"));
+        member.setPw(jsonobj.getString("pw"));
+        member.setName(jsonobj.getString("name"));
+
+        memberService.save(member);
+    }
+
+
 
 }
