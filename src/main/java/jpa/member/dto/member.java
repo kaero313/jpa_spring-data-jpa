071c01e4099eab2984adf91c027b4298b706f826
HOME <kaero313@naver.com>
2025-02-07 22:03:17 +0900

    연관관계의 주인은 외래키가 있는쪽이 주인이 됨(mappedBy를 사용하지 않음) = FK가 있는쪽이 연관관계의 주인

--- src/main/java/jpa/member/dto/member.java
+++ src/main/java/jpa/member/dto/member.java
@@ -19,6 +19,7 @@ public class member {
     @Column(name= "name")
     private String name;
 
+    // 연관관계의 주인은 외래키가 있는쪽이 주인이 됨(mappedBy를 사용하지 않음)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="name")
     private school school;
