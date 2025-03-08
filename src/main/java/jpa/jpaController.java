8cbdb1fa1b63ed714d2cafa206aba113772f2976
HOME <kaero313@naver.com>
2025-03-08 19:11:26 +0900

    Revert "select시 join하는 부분 수정"
    This reverts commit 0ca690d8f493f4d4a7e0df9750488c8be850a5c7.

--- src/main/java/jpa/jpaController.java
+++ src/main/java/jpa/jpaController.java
@@ -159,19 +159,15 @@ public class jpaController {
         Member member = new Member();
         member.setSchool(school);
 
-        JSONArray jsonary = new JSONArray(request);
 
-        List<Member> lists = new ArrayList<>();
+        member.setId("TestMemberId");
+        member.setPw("TestMemberPw");
+        member.setSchool(school);
 
-        for(int i=0; i<jsonary.length(); i++){
-            JSONObject jo = jsonary.getJSONObject(i);
-            member.setId(jo.getString("id"));
-            member.setPw(jo.getString("pw"));
-            member.setName(jo.getString("name"));
-            lists.add(member);
-        }
+        memberService.save(member);
 
-        memberService.saveAll(lists);
+        System.out.println(member);
+
 
         // 조회 결과가 n+1이 나오는 경우 테스트
 
